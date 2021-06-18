6143b83f7ea08adc6191ddae55d9548916adf2c0
LiFeng <2807229316@qq.com>
2021-06-18 15:52:24 +0800

    项目初始化

---
+++ src/main/java/edu/scu/csaserver/CsaServerApplication.java
@@ -0,0 +1,13 @@
+package edu.scu.csaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/scu/csaserver/CsaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.scu.csaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
