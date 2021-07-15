a832567967174655cde14f8c519519f7dbda77df
LiFeng <2807229316@qq.com>
2021-07-15 23:46:20 +0800

    关键节点(Linux可用了)

--- src/main/java/edu/scu/csaserver/utils/KeyNode.java
+++ src/main/java/edu/scu/csaserver/utils/KeyNode.java
@@ -49,10 +49,26 @@ public class KeyNode {
         if (osName.startsWith("Windows")) {
             filename = runDir + "\\KeyNode.dll";
         } else {
-            filename = runDir + "\\KeyNode.so";
+            // 路径分隔符还不一样
+            filename = runDir + "/KeyNode.so";
         }
         Runtime.getRuntime().load(filename);
 //        System.loadLibrary("KeyNode");
         return getKeyNode(nodeNum, links);
+    }
+
+    public static void main(String[] args) {
+        int[][] links = {
+                {2, 1}, {4, 1}, {6, 1}, {6, 8}, {8, 9},
+                {1, 3}, {9, 3}, {3, 7}, {3, 5}, {5, 10},
+                {10, 11}, {11, 13}, {11, 14}, {13, 15}, {13, 16},
+                {13, 17}, {15, 18}, {15, 20}, {16, 19}, {17, 18},
+                {12, 18}, {12, 17}, {14, 16}, {19, 20}
+        };
+        KeyNode keyNode = new KeyNode();
+        int[] keys = keyNode.getKeyNodeIds(20, links);
+        for (int key : keys) {
+            System.out.println(key);
+        }
     }
 }
